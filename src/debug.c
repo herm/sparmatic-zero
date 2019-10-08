9c8f61eeed0be693dde27b3f5a66c6c5a1b60b29
Hermann Kraus <hermr2d2+git@gmail.com>
2019-10-08 23:55:49 +0200

    Add interrupt debugging.

--- src/debug.c
+++ src/debug.c
@@ -44,3 +44,98 @@ void debugHex(uint16_t n)
     debugString(buf);
     debugString("\r\n");
 }
+
+#include <avr/interrupt.h>
+ISR(BADISR_vect)
+{
+    debugString("Bad ISR\r\n");
+}
+
+ISR( INT0_vect )
+{
+    debugString(" INT0_vect \r\n");
+}
+//ISR( PCINT0_vect )
+//{
+//    debugString(" PCINT0_vect \r\n");
+//}
+//ISR( PCINT1_vect )
+//{
+//    debugString(" PCINT1_vect \r\n");
+//}
+ISR( TIMER2_COMP_vect )
+{
+    debugString(" TIMER2_COMP_vect \r\n");
+}
+//ISR( TIMER2_OVF_vect )
+//{
+//    debugString(" TIMER2_OVF_vect \r\n");
+//}
+ISR( TIMER1_CAPT_vect )
+{
+    debugString(" TIMER1_CAPT_vect \r\n");
+}
+ISR( TIMER1_COMPA_vect )
+{
+    debugString(" TIMER1_COMPA_vect \r\n");
+}
+ISR( TIMER1_COMPB_vect )
+{
+    debugString(" TIMER1_COMPB_vect \r\n");
+}
+ISR( TIMER1_OVF_vect )
+{
+    debugString(" TIMER1_OVF_vect \r\n");
+}
+//ISR( TIMER0_COMP_vect )
+//{
+//    debugString(" TIMER0_COMP_vect \r\n");
+//}
+//ISR( TIMER0_OVF_vect )
+//{
+//    debugString(" TIMER0_OVF_vect \r\n");
+//}
+ISR( SPI_STC_vect )
+{
+    debugString(" SPI_STC_vect \r\n");
+}
+ISR( USART0_RX_vect )
+{
+    debugString(" USART0_RX_vect \r\n");
+}
+ISR( USART0_UDRE_vect )
+{
+    debugString(" USART0_UDRE_vect \r\n");
+}
+ISR( USART0_TX_vect )
+{
+    debugString(" USART0_TX_vect \r\n");
+}
+ISR( USI_START_vect )
+{
+    debugString(" USI_START_vect \r\n");
+}
+ISR( USI_OVERFLOW_vect )
+{
+    debugString(" USI_OVERFLOW_vect \r\n");
+}
+ISR( ANALOG_COMP_vect )
+{
+    debugString(" ANALOG_COMP_vect \r\n");
+}
+ISR( ADC_vect )
+{
+    debugString(" ADC_vect \r\n");
+}
+ISR( EE_READY_vect )
+{
+    debugString(" EE_READY_vect \r\n");
+}
+ISR( SPM_READY_vect )
+{
+    debugString(" SPM_READY_vect \r\n");
+}
+//ISR( LCD_vect)
+//{
+//    debugString(" LCD_vect\r\n");
+//}
